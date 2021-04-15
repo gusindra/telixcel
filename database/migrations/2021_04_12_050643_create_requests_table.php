cc6c240c7013caac5722273b4e3d262eb5859f5c
gusindra <gusin44@gmail.com>
2021-04-15 11:32:44 +0800

    add app

---
+++ database/migrations/2021_04_12_050643_create_requests_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRequestsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('requests', function (Blueprint $table) {
+            $table->id();
+            $table->string('source_id');
+            $table->text('context');
+            $table->string('media');
+            $table->string('from');
+            $table->string('identity');
+            $table->foreignId('user_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('requests');
+    }
+}
